725837a53ca6597af0e9cf0e386fb64304fe1739
Eric <ericwsp@icloud.com>
2026-04-19 16:22:50 -0700

    Minor Changes

--- sts2_char_portalcraftCode/Cards/StoneBreaker.cs
+++ sts2_char_portalcraftCode/Cards/StoneBreaker.cs
@@ -1,11 +1,11 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
 using sts2_char_portalcraft.sts2_char_portalcraftCode.Character;
 
 namespace sts2_char_portalcraft.sts2_char_portalcraftCode.Cards;
@@ -15,29 +15,23 @@ public sealed class StoneBreaker : sts2_char_portalcraftCard
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => new DynamicVar[]
     {
-        new IntVar("MagicNumber", 8m),
+        new DamageVar(1m, ValueProp.Move),
+        new RepeatVar(8),
     };
 
-    public StoneBreaker() : base(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self) { }
+    public StoneBreaker() : base(1, CardType.Attack, CardRarity.Uncommon, TargetType.RandomEnemy) { }
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        int hits = (int)DynamicVars["MagicNumber"].BaseValue;
-        for (int i = 0; i < hits; i++)
-        {
-            var enemies = CombatState.HittableEnemies.ToList();
-            if (enemies.Count == 0) return;
-
-            var target = Owner.RunState.Rng.Shuffle.NextItem(enemies);
-            await DamageCmd.Attack(1m)
-                .FromCard(this)
-                .Targeting(target)
-                .Execute(choiceContext);
-        }
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .WithHitCount(DynamicVars.Repeat.IntValue)
+            .FromCard(this)
+            .TargetingRandomOpponents(CombatState)
+            .Execute(choiceContext);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars["MagicNumber"].UpgradeValueBy(4m);
+        DynamicVars.Repeat.UpgradeValueBy(4m);
     }
 }
